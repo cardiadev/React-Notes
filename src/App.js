f05a43ea60d3facc71cba1b6af2297101b2bb251
Carlos Diaz <hola@cjdiaz.com>
2019-06-18 20:48:58 -0500

    Setup proyect and creating text filds with Material-UI

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Typography from "@material-ui/core/Typography";
+import Grid from "@material-ui/core/Grid";
+import TextField from "@material-ui/core/TextField";
+import Fab from "@material-ui/core/Fab";
+import Icon from "@material-ui/core/Icon";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "",
+      description: "",
+      notes: []
+    };
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <Typography align="center" variant="h2" gutterBottom>
+          My Notes
+        </Typography>
+        <Grid container justify="center" spacing={2}>
+          <Grid item xs={8}>
+            <Grid item xs={12}>
+              <TextField
+                type="text"
+                placeholder="Title for this note…"
+                margin="normal"
+                fullWidth
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                placeholder="Start taking notes…"
+                margin="normal"
+                multiline
+                rows="4"
+                fullWidth
+              />
+            </Grid>
+            <Fab color="secondary">
+              <Icon>edit_icon</Icon>
+            </Fab>
+          </Grid>
+        </Grid>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
